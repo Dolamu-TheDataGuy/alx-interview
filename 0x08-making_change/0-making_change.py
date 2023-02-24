e8ac4baf5cfe48b1a095dff6fd25b5d66b67bb83
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-02-24 22:46:58 +0100

    made changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-
 """
-Making change
+Making Change
 """
 
 
 def makeChange(coins, total):
     """
-    Returns the minimum number of coins needed to meet a given total
+    Return the minimum number of coins needed to meet a given total
     Args:
         coins (list of ints): a list of coins of different values
         total (int): total value to be met
-    Returns:
-    Number of coins or -1 if meeting the total is not possible    
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
-
     if total <= 0:
         return 0
     if coins == [] or coins is None:
@@ -27,17 +25,17 @@ def makeChange(coins, total):
 
     coins.sort(reverse=True)
     coin_count = 0
-    for coin in coins:
-        if total % 1 == 0:
-            coin_count += int(total/coin)
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
             return coin_count
-        if total - coin >= 0:
-            if int(total/coin) > 1:
-                coin_count += int(total/coin)
-                total = total % coin
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
             else:
                 coin_count += 1
-                total -= 1
+                total -= i
                 if total == 0:
                     break
     if total > 0:
